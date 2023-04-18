7889aeba6a45adc9b4c2a54983e281c8cc2ac095
diegomitari <diego89ma@gmail.com>
2023-04-17 21:20:16 -0500

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+import react from "react"
+import { createRoot } from "react-dom/client"
+
+import Home from "./components/Home"
+
+const dom = document.getElementById("root")
+
+const root = createRoot(dom)
+root.render(<Home />)
